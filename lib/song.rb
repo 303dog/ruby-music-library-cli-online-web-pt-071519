b1d949d8bdbf5f84f1051645017e62a5d9bb5e1b
Paul Beckwith <303dog@gmail.com>
2019-09-07 20:09:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,9 +37,9 @@ class Song
       self.create(song_name, artist, genre)
     end
     
-    def self.create_from_filename(filename)
+#    def self.create_from_filename(filename)
       self.all.detect{|o| o.name == name}
-    end
+#    end
       
     def self.destroy_all
         @@all.clear
